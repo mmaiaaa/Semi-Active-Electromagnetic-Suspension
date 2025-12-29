7d82c8d2e15bc5b3703310bbbc8d69bb66cdc418
maia <maya.alasbahi@outlook.com>
2025-12-29 19:49:36 +0400

    Update run_pid.m

--- simulations/run_pid.m
+++ simulations/run_pid.m
@@ -1,1 +1,17 @@
+clc; close all;
 
+[A, B, ~, C_acc, D_acc] = state_space_quarter_car();
+[T, z_g, z_gdot] = road_profile_half_sine();
+
+Kpdef = 7500;
+Kddef = 6300;
+Ki    = 5300;
+Kacc  = 500;
+F_max = 2000;
+
+x0 = zeros(5,1);
+
+odefun = @(t,x) state_deriv_pid(t,x,A,B,T,z_g,z_gdot,...
+                                Kpdef,Kddef,Ki,Kacc,F_max,C_acc,D_acc);
+
+[tt,XX] = ode45(odefun,T,x0);
