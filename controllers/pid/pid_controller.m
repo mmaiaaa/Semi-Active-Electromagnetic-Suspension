e1a71f19e169db310e3d14189c0dd1d4dc9e88b8
maia <maya.alasbahi@outlook.com>
2025-12-29 20:08:26 +0400

    Update pid_controller.m

--- controllers/pid/pid_controller.m
+++ controllers/pid/pid_controller.m
@@ -1,12 +1,25 @@
-function F_em = pid_controller(x, zg, zgdot, Kpdef, Kddef, Ki, Kacc, Fm, C_acc, D_acc)
+x0 = zeros(5,1);  % [z_b; z_t; z_bdot; z_tdot; integral_defl]
 
-susp_defl = x(1) - x(2);
-defl_dot  = x(3) - x(4);
-int_defl  = x(5);
+%% ACTIVE SYSTEM
+odefun = @(t,x) state_deriv_PID(t,x,A,B,T,z_g,z_gdot,Kpdef,Kddef,Ki,Kacc,F_max,C_acc,D_acc);
+[tt,XX] = ode45(odefun,T,x0);
 
-a_b = C_acc*x(1:4) + D_acc*[0; zg; zgdot];
+z_b_act    = XX(:,1); 
+z_t_act    = XX(:,2);
+z_bdot_act = XX(:,3); 
+z_tdot_act = XX(:,4);
+defl_act   = z_b_act - z_t_act;
+tire_defl_act = z_t_act - interp1(T,z_g,tt);
 
-F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Ki*int_defl - Kacc*a_b;
-
-F_em = max(min(F_raw,Fm),-Fm);
+%% ACTIVE VAR
+F_hist = zeros(length(tt),1);
+a_b_act = zeros(length(tt),1);
+for i=1:length(tt)
+    susp_defl = XX(i,1) - XX(i,2);
+    defl_dot  = XX(i,3) - XX(i,4);
+    int_defl  = XX(i,5);
+    a_b = C_acc*XX(i,1:4)' + D_acc*[0; z_g(i); z_gdot(i)];
+    F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Ki*int_defl - Kacc*a_b;
+    F_hist(i) = max(min(F_raw,F_max),-F_max);
+    a_b_act(i) = a_b;
 end
