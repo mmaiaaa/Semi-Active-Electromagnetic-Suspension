f733068a20537c51823a376aec201bb8ffa609f7
maia <maya.alasbahi@outlook.com>
2025-12-29 20:23:59 +0400

    Update run_pid.m

--- simulations/run_pid.m
+++ simulations/run_pid.m
@@ -3,18 +3,31 @@ clc; close all;
 addpath('../model');
 addpath('../controllers/pid');
 
+% SYSTEM
 [A, B, ~, C_acc, D_acc] = state_space_quarter_car();
 [T, z_g, z_gdot] = road_profile_half_sine();
 
+% CONTROLLER PARAMETERS
 Kpdef = 7500;
 Kddef = 6300;
 Ki    = 5300;
 Kacc  = 500;
 F_max = 2000;
 
+% INITIAL CONDITION
 x0 = zeros(5,1);
 
-odefun = @(t,x) state_deriv_pid(t,x,A,B,T,z_g,z_gdot,...
-                                Kpdef,Kddef,Ki,Kacc,F_max,C_acc,D_acc);
+% SIMULATION
+odefun = @(t,x) state_deriv_PID(t,x,A,B,T,z_g,z_gdot,Kpdef,Kddef,Ki,Kacc,F_max,C_acc,D_acc);
+[tt, XX] = ode45(odefun, T, x0);
 
-[tt,XX] = ode45(odefun,T,x0);
+% PID controller
+[F_hist, a_b_act] = pid_controller(XX, T, z_g, z_gdot, Kpdef, Kddef, Ki, Kacc, F_max, C_acc, D_acc);
+
+% PLOT
+z_b_act    = XX(:,1); 
+z_t_act    = XX(:,2);
+z_bdot_act = XX(:,3); 
+z_tdot_act = XX(:,4);
+defl_act   = z_b_act - z_t_act;
+tire_defl_act = z_t_act - interp1(T,z_g,tt);
