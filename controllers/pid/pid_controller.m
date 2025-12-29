da2dfc615d07525dc5b8da91cc873f27972e4222
maia <maya.alasbahi@outlook.com>
2025-12-29 20:23:13 +0400

    Update pid_controller.m

--- controllers/pid/pid_controller.m
+++ controllers/pid/pid_controller.m
@@ -1,19 +1,17 @@
-z_b_act    = XX(:,1); 
-z_t_act    = XX(:,2);
-z_bdot_act = XX(:,3); 
-z_tdot_act = XX(:,4);
-defl_act   = z_b_act - z_t_act;
-tire_defl_act = z_t_act - interp1(T,z_g,tt);
+function [F_hist, a_b_act] = pid_controller(XX, T, z_g, z_gdot, Kpdef, Kddef, Ki, Kacc, F_max, C_acc, D_acc)
+    N = size(XX,1);
+    F_hist = zeros(N,1);
+    a_b_act = zeros(N,1);
 
-%% ACTIVE VAR
-F_hist = zeros(length(tt),1);
-a_b_act = zeros(length(tt),1);
-for i=1:length(tt)
-    susp_defl = XX(i,1) - XX(i,2);
-    defl_dot  = XX(i,3) - XX(i,4);
-    int_defl  = XX(i,5);
-    a_b = C_acc*XX(i,1:4)' + D_acc*[0; z_g(i); z_gdot(i)];
-    F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Ki*int_defl - Kacc*a_b;
-    F_hist(i) = max(min(F_raw,F_max),-F_max);
-    a_b_act(i) = a_b;
+    for i = 1:N
+        susp_defl = XX(i,1) - XX(i,2);
+        defl_dot  = XX(i,3) - XX(i,4);
+        int_defl  = XX(i,5);
+
+        a_b = C_acc*XX(i,1:4)' + D_acc*[0; z_g(i); z_gdot(i)];
+        F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Ki*int_defl - Kacc*a_b;
+
+        F_hist(i) = max(min(F_raw, F_max), -F_max);
+        a_b_act(i) = a_b;
+    end
 end
