a9d9a286338ce465b8031ec10f87029f81c79b66
maia <maya.alasbahi@outlook.com>
2025-12-29 19:40:00 +0400

    Update parameters.m

--- model/parameters.m
+++ model/parameters.m
@@ -1,1 +1,16 @@
+%% Quarter-Car Physical Parameters
 
+% Masses
+m_b = 460.5;     % Sprung mass (kg)
+m_t = 13;        % Unsprung mass (kg)
+
+% Suspension
+k_b = 34000;     % Suspension stiffness (N/m)
+c_b = 3660;      % Suspension damping (Ns/m)
+
+% Tire
+k_t = 30000;     % Tire stiffness (N/m)
+c_t = 451;       % Tire damping (Ns/m)
+
+% Actuator limit (used by controllers)
+F_max = 2000;    % N
