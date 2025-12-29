06177189b448ecfbc6826df1f567c8da4b7973a7
maia <maya.alasbahi@outlook.com>
2025-12-29 20:05:29 +0400

    Update state_deriv_pid.m

--- controllers/pid/state_deriv_pid.m
+++ controllers/pid/state_deriv_pid.m
@@ -1,15 +1,37 @@
-function dx = state_deriv_pid(t,x,A,B,Tvec,zg_vec,zgdot_vec,Kpdef,Kddef,Ki,Kacc,Fm,C_acc,D_acc)
+function dx = state_deriv_PID(t,x,A,B,Tvec,zg_vec,zgdot_vec,Kpdef,Kddef,Ki,Kacc,Fm,C_acc,D_acc)
+    % x = [z_b; z_t; z_bdot; z_tdot; integral_defl]
 
-[~, idx] = min(abs(Tvec - t));
-zg    = zg_vec(idx);
-zgdot = zgdot_vec(idx);
+    % Find closest index in time vector
+    [~, idx] = min(abs(Tvec - t));
+    zg = zg_vec(idx); 
+    zgdot = zgdot_vec(idx);
 
-F_em = pid_controller(x, zg, zgdot, Kpdef, Kddef, Ki, Kacc, Fm, C_acc, D_acc);
+    % Suspension deflection and rate
+    susp_defl = x(1) - x(2);
+    defl_dot  = x(3) - x(4);
 
-u = [F_em; zg; zgdot];
+    % Integral of deflection
+    int_defl = x(5);
 
-dx_main = A*x(1:4) + B*u;
-dx_int  = x(1) - x(2);
+    % Approximate body acceleration
+    a_b = C_acc*x(1:4) + D_acc*[0; zg; zgdot];
 
-dx = [dx_main; dx_int];
+    % PID + acceleration feedback
+    F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Ki*int_defl - Kacc*a_b;
+
+    % Saturate actuator
+    F_em = max(min(F_raw,Fm),-Fm);
+
+    % System input
+    u = [F_em; zg; zgdot];
+
+    % State derivative
+    dx_main = A*x(1:4) + B*u;
+
+    % Integral derivative
+    dx_int = susp_defl;
+
+    % Combine derivatives
+    dx = [dx_main; dx_int];
 end
+
