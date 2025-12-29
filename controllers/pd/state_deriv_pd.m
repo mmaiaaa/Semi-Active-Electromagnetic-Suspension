dda03d3cfd90148b641928cc43aa5d9141534c9d
maia <maya.alasbahi@outlook.com>
2025-12-29 20:01:41 +0400

    Update state_deriv_pd.m

--- controllers/pd/state_deriv_pd.m
+++ controllers/pd/state_deriv_pd.m
@@ -1,11 +1,27 @@
 function dx = state_deriv_pd(t,x,A,B,Tvec,zg_vec,zgdot_vec,Kpdef,Kddef,Kacc,Fm,C_acc,D_acc)
+% Mixed-feedback PD + small acceleration
 
-[~, idx] = min(abs(Tvec - t));
-zg    = zg_vec(idx);
-zgdot = zgdot_vec(idx);
+    % Find closest index in time vector
+    [~, idx] = min(abs(Tvec - t));
+    zg = zg_vec(idx); 
+    zgdot = zgdot_vec(idx);
 
-F_em = pd_controller(x, zg, zgdot, Kpdef, Kddef, Kacc, Fm, C_acc, D_acc);
+    % Suspension deflection and rate
+    susp_defl = x(1) - x(2);
+    defl_dot  = x(3) - x(4);
 
-u  = [F_em; zg; zgdot];
-dx = A*x + B*u;
+    % Approximate body acceleration
+    a_b = C_acc*x + D_acc*[0; zg; zgdot];
+
+    % Mixed PD + acceleration feedback
+    F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Kacc*a_b;
+
+    % Saturate actuator
+    F_em = max(min(F_raw,Fm),-Fm);
+
+    % System input
+    u = [F_em; zg; zgdot];
+
+    % State derivative
+    dx = A*x + B*u;
 end
