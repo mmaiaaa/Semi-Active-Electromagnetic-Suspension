b3c4a1770f6bf8195c77343086b588f452ef3d10
maia <maya.alasbahi@outlook.com>
2025-12-29 20:34:49 +0400

    Update pd_controller.m

--- controllers/pd/pd_controller.m
+++ controllers/pd/pd_controller.m
@@ -1,12 +1,18 @@
-function F_em = pd_controller(x, zg, zgdot, Kpdef, Kddef, Kacc, Fm, C_acc, D_acc)
-% Mixed-feedback PD + acceleration feedback
+function [F_hist, a_b_act] = pd_controller(XX, T, z_g, z_gdot, Kpdef, Kddef, Kacc, F_max, C_acc, D_acc)
+% Compute actuator force and body acceleration for PD active suspension
 
-susp_defl = x(1) - x(2);
-defl_dot  = x(3) - x(4);
+    N = size(XX,1);
+    F_hist = zeros(N,1);
+    a_b_act = zeros(N,1);
 
-a_b = C_acc*x + D_acc*[0; zg; zgdot];
+    for i = 1:N
+        susp_defl = XX(i,1) - XX(i,2);
+        defl_dot  = XX(i,3) - XX(i,4);
 
-F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Kacc*a_b;
-F_em  = max(min(F_raw,Fm),-Fm);
+        a_b = C_acc*XX(i,:)' + D_acc*[0; z_g(i); z_gdot(i)];
+        F_raw = -Kpdef*susp_defl - Kddef*defl_dot - Kacc*a_b;
+
+        F_hist(i) = max(min(F_raw,F_max), -F_max);
+        a_b_act(i) = a_b;
+    end
 end
-
